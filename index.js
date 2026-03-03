7623ac1e7c64bf064c67480e7a1b4e1234bc71ff
Ivan-Malgin <i.v.malgin2006@gmail.com>
2026-03-03 20:00:26 +0500

    important

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const todo = []
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -12,10 +13,30 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    for (const file of files) {
+        for (const str of file.split('\n')){
+            const idx = str.indexOf('// TODO ')
+            if (idx !== -1) {
+                todo.push(str.slice(idx));
+            }
+        }
+    }
+
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'important':
+            let important_commands = []
+            let commands = todo
+            for(let command of commands) {
+                if(command.indexOf('!')) {
+                    important_commands.push(command)
+                }
+            }
+            for(let n = 0; n < important_commands.length; n++) {
+                console.log(important_commands[n])
+            }
         default:
             console.log('wrong command');
             break;
