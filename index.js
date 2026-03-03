b41d3e2ef1848da5af3bdc588b8d44bdc23a5c91
aleksandraizmaylova <aleksandraizmaylova111@gmail.com>
2026-03-03 19:43:43 +0500

    1 task

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const todo = []
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -12,6 +13,15 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    for (const file of files) {
+        for (const str of file.split('\n')){
+            const idx = str.indexOf('// TODO ')
+            if (idx !== -1) {
+                todo.push(str.slice(idx));
+            }
+        }
+    }
+
     switch (command) {
         case 'exit':
             process.exit(0);
